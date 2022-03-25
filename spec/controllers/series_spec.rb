4994a936125f298269290585cbdb8faf3e8b46a0
Sibgha <sibgha@Sibghas-MacBook-Pro.local>
2022-03-25 16:54:13 +0500

    Rubocop fix

--- spec/controllers/series_spec.rb
+++ spec/controllers/series_spec.rb
@@ -1,22 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe SeriesController, :type => :controller do
-
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+RSpec.describe SeriesController, type: :controller do
+  let(:valid_attributes) do
+    skip('Add a hash of attributes valid for your model')
+  end
 
   let(:valid_session) { {} }
 
-
-  describe "GET index" do
-    it "assigns all series as @series" do
+  describe 'GET index' do
+    it 'assigns all series as @series' do
       series = Series.create! valid_attributes
       get :index, {}, valid_session
       expect(assigns(:series)).to eq([series])
     end
   end
-
-  
-
 end
