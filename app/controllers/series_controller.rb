f4a7decd3e3742ec8514220f0bacb01bd13d9e3e
Sibgha <sibgha@Sibghas-MacBook-Pro.local>
2022-03-25 16:16:24 +0500

    Added search

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -2,6 +2,12 @@
 
 class SeriesController < ApplicationController
   def index
-    @series = Series.all
+    @series = Series.search(series_params[:term])
+  end
+
+  private
+
+  def series_params
+    params.permit(:term)
   end
 end
--- app/models/series.rb
+++ app/models/series.rb
@@ -2,4 +2,12 @@
 
 class Series < ApplicationRecord
   has_and_belongs_to_many :reviews
+
+  def self.search(term)
+    if term.blank?
+      all
+    else
+      where('actor LIKE ?', "%#{term}%")
+    end
+  end
 end
