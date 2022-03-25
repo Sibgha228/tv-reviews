7e4e5c29bc095c350688146db95ad3adc7bdee52
Sibgha <sibgha@Sibghas-MacBook-Pro.local>
2022-03-25 16:53:48 +0500

    Controller test

---
+++ spec/controllers/series_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe SeriesController, :type => :controller do
+
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
+
+  let(:valid_session) { {} }
+
+
+  describe "GET index" do
+    it "assigns all series as @series" do
+      series = Series.create! valid_attributes
+      get :index, {}, valid_session
+      expect(assigns(:series)).to eq([series])
+    end
+  end
+
+  
+
+end
